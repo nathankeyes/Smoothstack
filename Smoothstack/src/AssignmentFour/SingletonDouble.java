6c3ce5cde6157ca5968bf4e7965e762e6d07ddad
Nathan Keyes <47252688+keyesn@users.noreply.github.com>
2021-03-12 08:06:32 -0600

    Implemented a double lock for a simpleton class

---
+++ Smoothstack/src/AssignmentFour/SingletonDouble.java
@@ -0,0 +1,45 @@
+package AssignmentFour;
+
+/**
+ * @author      Nathan Keyes
+ * @DateCreated 3/12/21
+ * @LastEdited  3/12/21
+ * @Description Implement Singleton with double checked locking
+ */
+
+public class SingletonDouble {
+	
+	volatile public static SingletonDouble instance = null;
+	
+	private SingletonDouble() { }
+	
+	
+	
+	public static SingletonDouble getInstance() {
+		
+		// do an initial if check to see if we even need to lock up with synch (first check)
+		if ( instance == null ) {
+			// now do the same process, with the original check being the second check
+			synchronized (instance) {
+				if ( instance == null )
+					instance = new SingletonDouble();
+			}
+			
+		}
+		
+		/*
+		// standard way (shown in Pramod's tutorial)
+		synchronized (instance) {
+			if ( instance == null )
+				instance = new SingletonDouble();
+		}
+		*/
+		return instance;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
